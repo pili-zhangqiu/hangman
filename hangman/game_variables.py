8b43af027ae08378e94f263a1af8ebadf1a8c639
pili-zhangqiu <p.zhangqiu@gmail.com>
2023-10-12 00:35:04 +0100

    feat(random-pick): Choose a random word from the list

--- hangman/game_variables.py
+++ hangman/game_variables.py
@@ -1,8 +1,12 @@
 """
 WIP: This file is used to test the creation of variables for the game.
 """
+import random
 
-# Task 2: Define list of possible words
+# Task 1: Define list of possible words
 word_list = ['apple', 'banana', 'cherry', 'dragon fruit', 'pear']
-
 print(word_list)
+
+# Task 2: Choose a random word from the list
+word = random.choice(word_list)
+print(word)
