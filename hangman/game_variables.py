57811c1bac909534efcf22482edac355d159374a
pili-zhangqiu <p.zhangqiu@gmail.com>
2023-10-12 00:41:47 +0100

    feat(user-input): Ask user to input a letter in command line and validate answer

--- hangman/game_variables.py
+++ hangman/game_variables.py
@@ -2,6 +2,7 @@
 WIP: This file is used to test the creation of variables for the game.
 """
 import random
+import string
 
 # Task 1: Define list of possible words
 word_list = ['apple', 'banana', 'cherry', 'dragon fruit', 'pear']
@@ -10,3 +11,12 @@ print(word_list)
 # Task 2: Choose a random word from the list
 word = random.choice(word_list)
 print(word)
+
+# Task 3: Ask the user for an input
+guess = input("Enter a guess (i.e. letter): ") 
+
+# Task 4: Check that the input is a single letter character
+while len(guess) != 1 or guess not in string.ascii_letters:
+    guess = input("Invalid input! Please, enter a single letter:") 
+
+print(f"Good guess! You've chosen: {guess}")
