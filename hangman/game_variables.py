5b340b2fc3eb513b983e9d9e2944bd55fe0198b3
pili-zhangqiu <p.zhangqiu@gmail.com>
2023-10-11 23:59:51 +0100

    feat(ls): Create list of words for game

---
+++ hangman/game_variables.py
@@ -0,0 +1,8 @@
+"""
+WIP: This file is used to test the creation of variables for the game.
+"""
+
+# Task 2: Define list of possible words
+word_list = ['apple', 'banana', 'cherry', 'dragon fruit', 'pear']
+
+print(word_list)
